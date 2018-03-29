9a0a0b66a6a4dc6d437981902e1061e3ba340676
develop-build <anubhavj1357@gmail.com>
2018-03-29 14:57:30 +0000

    Article Update functionality added

--- rubyOnRails/blog/app/controllers/articles_controller.rb
+++ rubyOnRails/blog/app/controllers/articles_controller.rb
@@ -2,6 +2,10 @@ class ArticlesController < ApplicationController
     def new
 		@article = Article.new
     end
+	
+	def edit
+		@article = Article.find(params[:id])
+	end
 
     def	create
         @article=Article.new(article_params)
@@ -12,6 +16,17 @@ class ArticlesController < ApplicationController
 			render 'new'
 		end
     end
+
+    def	update
+        @article=Article.find(params[:id])
+       
+        if @article.update(article_params)
+			redirect_to @article
+		else
+			render 'edit'
+		end
+    end
+
 
 	def show
 		@article = Article.find(params[:id])
